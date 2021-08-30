4c79f389f2f065197e8af41b6d71ee933a2fc328
etasan <81860224+etasan@users.noreply.github.com>
2021-08-31 00:21:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,21 +14,21 @@ function slide() {
         btn1.classList.add('btnlinkClicked');
         btn2.classList.remove('btnlinkClicked');
         btn3.classList.remove('btnlinkClicked');
-        imagePlayer.style.setProperty('--imageTab', 'url(/images/illustration-features-tab-1.svg)');
+        imagePlayer.style.setProperty('--imageTab', 'url(images/illustration-features-tab-1.svg)');
         titleOne.innerHTML = 'Bookmark in one click';
         textOne.innerHTML = 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.'
     } else if (buttonPressed === "speedy") {
         btn1.classList.remove('btnlinkClicked');
         btn2.classList.add('btnlinkClicked');
         btn3.classList.remove('btnlinkClicked');
-        imagePlayer.style.setProperty('--imageTab', 'url(/images/illustration-features-tab-2.svg)');
+        imagePlayer.style.setProperty('--imageTab', 'url(images/illustration-features-tab-2.svg)');
         titleOne.innerHTML = 'Intelligent search';
         textOne.innerHTML = 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.'
     } else {
         btn1.classList.remove('btnlinkClicked');
         btn2.classList.remove('btnlinkClicked');
         btn3.classList.add('btnlinkClicked');
-        imagePlayer.style.setProperty('--imageTab', 'url(/images/illustration-features-tab-3.svg)');
+        imagePlayer.style.setProperty('--imageTab', 'url(images/illustration-features-tab-3.svg)');
         titleOne.innerHTML = 'Share your bookmarks';
         textOne.innerHTML = 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.'
     }
